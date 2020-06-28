85bcccbd83771907f696f3d061875eb7f83a6c7a
ferdianafsa <farisafza201002@gmail.com>
2020-06-28 20:53:52 +0700

    add index

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
   get 'teacher/index'
   get 'homes/index'
   get "/teacher/index" => "teacher/index"
+  # get "/artikels" => "artikels/index"
   root to: 'homes#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
